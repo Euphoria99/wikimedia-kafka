e6feab6ad39aed8ce88c99d1e79bda7b211e5dca
Euphoria99 <363pavanbhat@gmail.com>
2025-10-15 20:08:35 +0530

    Kafka Consumer Init

---
+++ Kafka-Database-Consumer/src/main/java/dev/pavanbhat/Kafka_Database_Consumer/SpringBootConsumerApplication.java
@@ -0,0 +1,13 @@
+package dev.pavanbhat.Kafka_Database_Consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConsumerApplication.class, args);
+	}
+
+}
