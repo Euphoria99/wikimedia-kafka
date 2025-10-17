9f0cf0fea24ddbdcf83928f8055e96b8ce42099c
Euphoria99 <363pavanbhat@gmail.com>
2025-10-17 23:17:59 +0530

    Wikimedia Data Entity

---
+++ Kafka-Database-Consumer/src/main/java/dev/pavanbhat/Kafka_Database_Consumer/entity/WikimediaData.java
@@ -0,0 +1,20 @@
+package dev.pavanbhat.Kafka_Database_Consumer.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name ="wikimedia_recentchange")
+@Getter
+@Setter
+public class WikimediaData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private String wikiEventData;
+
+}
